3c01b70286ee95fd1ca4d47dee51d4e2dfb71349
Hanami <64640200+HanamiMirai@users.noreply.github.com>
2022-04-12 03:14:13 +0800

    Add files via upload

---
+++ int.py
@@ -0,0 +1,26 @@
+#ing
+number = 5
+numberAge = 18
+
+#float
+fnumber = 5.7
+
+#str
+name = "hanami"
+
+#boot
+status = True
+
+#Ptint
+print(name)
+
+#Эуранирование
+print("Он \"плохой\" человек")
+
+#Перевод строки
+print('Привет \n мир')
+
+#Конкатенация
+print("Привет, меня зовут " + name + "!")
+print("Мне " + str(numberAge) + " лет!")
+
